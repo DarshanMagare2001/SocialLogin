8583719c5ca6a2474c2c1ed62c016721ab1c3cc0
Darshan <>
2023-06-21 11:59:21 +0530

    new changes

--- SocialLogin/SocialLogin/AppDelegate.swift
+++ SocialLogin/SocialLogin/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import GoogleSignIn
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        GIDSignIn.sharedInstance().clientID = "894353290979-gaim6ageh4m88qa7kp3aq28hhuci4vc1.apps.googleusercontent.com"
         return true
     }
 
