4152fe165a84a2ce6afc14baa8444beeffb08292
Darshan <>
2023-06-21 11:26:15 +0530

    new changes

---
+++ SocialLogin/SocialLogin/Controller/MainVC.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  SocialLogin
+//
+//  Created by IPS-161 on 21/06/23.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func signInWithGoogleBtnPressed(_ sender: UIButton) {
+        
+    }
+    
+    
+    @IBAction func signInWithFacebookBtnPressed(_ sender: UIButton) {
+        
+    }
+    
+    
+    @IBAction func signInWithAppleBtnPressed(_ sender: UIButton) {
+        
+        
+    }
+    
+}
+
