2da54089ea1f80f2884175082adc705b03585813
Darshan <>
2023-06-22 11:50:18 +0530

    new changes

--- SocialLogin/SocialLogin/Controller/MainVC.swift
+++ SocialLogin/SocialLogin/Controller/MainVC.swift
@@ -5,27 +5,25 @@ import FacebookLogin
 import AuthenticationServices
 
 class MainVC: UIViewController, GIDSignInDelegate, LoginButtonDelegate {
-    
-    
+    @IBOutlet weak var activityIndicatorView: UIView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        activityIndicatorView.isHidden = true
+        activityIndicator.startAnimating()
         GIDSignIn.sharedInstance()?.delegate = self
         
     }
     
-    
-    
-    
-    
-    
-    
     @IBAction func signInWithGoogleBtnPressed(_ sender: UIButton) {
         GIDSignIn.sharedInstance()?.presentingViewController = self
         GIDSignIn.sharedInstance()?.signIn()
+        activityIndicatorView.isHidden = false
     }
     
     @IBAction func signInWithFacebookBtnPressed(_ sender: UIButton) {
         loginButtonClicked()
+        activityIndicatorView.isHidden = false
     }
     
     @IBAction func signInWithAppleBtnPressed(_ sender: UIButton) {
@@ -37,10 +35,12 @@ class MainVC: UIViewController, GIDSignInDelegate, LoginButtonDelegate {
         authorizationController.delegate = self
         authorizationController.presentationContextProvider = self
         authorizationController.performRequests()
+        activityIndicatorView.isHidden = false
     }
     
     func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
         if error == nil {
+            activityIndicatorView.isHidden = true
             guard let data = user.profile else { return }
             guard let profilePictureURL = user.profile.imageURL(withDimension: 200)?.absoluteString else { return }
             let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
@@ -60,6 +60,7 @@ class MainVC: UIViewController, GIDSignInDelegate, LoginButtonDelegate {
                 if let error = error {
                     print("Error fetching Facebook user profile: \(error.localizedDescription)")
                 } else if let userData = result as? [String: Any] {
+                    self.activityIndicatorView.isHidden = true
                     let name = userData["name"] as? String
                     let userID = userData["id"] as? String
                     let email = userData["email"] as? String
@@ -109,12 +110,11 @@ class MainVC: UIViewController, GIDSignInDelegate, LoginButtonDelegate {
 extension MainVC: ASAuthorizationControllerDelegate {
     func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
         if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
+            activityIndicatorView.isHidden = true
             let userID = appleIDCredential.user
             let email = appleIDCredential.email
             let fullName = appleIDCredential.fullName
-            
             let name = "\(fullName?.givenName ?? "") \(fullName?.familyName ?? "")"
-            
             let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
             let destinationVC = storyBoard.instantiateViewController(withIdentifier: "DetailVC") as! DetailVC
             destinationVC.name = name
@@ -127,6 +127,7 @@ extension MainVC: ASAuthorizationControllerDelegate {
     
     func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
         print("Apple sign-in error: \(error.localizedDescription)")
+        activityIndicatorView.isHidden = true
     }
 }
 
