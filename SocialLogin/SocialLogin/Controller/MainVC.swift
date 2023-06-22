eeaffc9583535879735db70f14392b7cafef2950
Darshan <>
2023-06-22 10:43:46 +0530

    new changes

--- SocialLogin/SocialLogin/Controller/MainVC.swift
+++ SocialLogin/SocialLogin/Controller/MainVC.swift
@@ -8,20 +8,22 @@ class MainVC: UIViewController, GIDSignInDelegate, LoginButtonDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         GIDSignIn.sharedInstance()?.delegate = self
+        
     }
-
+    
     @IBAction func signInWithGoogleBtnPressed(_ sender: UIButton) {
         GIDSignIn.sharedInstance()?.presentingViewController = self
         GIDSignIn.sharedInstance()?.signIn()
     }
-
+    
     @IBAction func signInWithFacebookBtnPressed(_ sender: UIButton) {
         let loginManager = LoginManager()
-        loginManager.logIn(permissions: ["public_profile", "email"], viewController: self) { result in
+        loginManager.logIn(permissions: ["email"], viewController: self) { result in
             switch result {
             case .success(let grantedPermissions, _, _):
                 if grantedPermissions.contains("email") {
                     self.fetchFacebookUserProfile()
+                    // TODO: Handle publishing content with the "email" permission
                 } else {
                     print("Email permission not granted.")
                 }
@@ -32,18 +34,18 @@ class MainVC: UIViewController, GIDSignInDelegate, LoginButtonDelegate {
             }
         }
     }
-
+    
     @IBAction func signInWithAppleBtnPressed(_ sender: UIButton) {
         let appleIDProvider = ASAuthorizationAppleIDProvider()
         let request = appleIDProvider.createRequest()
         request.requestedScopes = [.fullName, .email]
-
+        
         let authorizationController = ASAuthorizationController(authorizationRequests: [request])
         authorizationController.delegate = self
         authorizationController.presentationContextProvider = self
         authorizationController.performRequests()
     }
-
+    
     func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
         if error == nil {
             guard let data = user.profile else { return }
@@ -57,7 +59,7 @@ class MainVC: UIViewController, GIDSignInDelegate, LoginButtonDelegate {
             navigationController?.pushViewController(destinationVC, animated: true)
         }
     }
-
+    
     func fetchFacebookUserProfile() {
         if let accessToken = AccessToken.current, !accessToken.isExpired {
             let graphRequest = GraphRequest(graphPath: "me", parameters: ["fields": "id,name,email,picture.width(200).height(200)"], tokenString: accessToken.tokenString, version: nil, httpMethod: .get)
@@ -68,7 +70,7 @@ class MainVC: UIViewController, GIDSignInDelegate, LoginButtonDelegate {
                     let name = userData["name"] as? String
                     let userID = userData["id"] as? String
                     let email = userData["email"] as? String
-
+                    
                     if let pictureData = userData["picture"] as? [String: Any], let picture = pictureData["data"] as? [String: Any], let profilePictureURL = picture["url"] as? String {
                         let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
                         let destinationVC = storyBoard.instantiateViewController(withIdentifier: "DetailVC") as! DetailVC
@@ -82,15 +84,16 @@ class MainVC: UIViewController, GIDSignInDelegate, LoginButtonDelegate {
             }
         }
     }
-
+    
     func loginButtonDidLogOut(_ loginButton: FBLoginButton) {
         // Handle Facebook logout
     }
-
+    
     func loginButton(_ loginButton: FBLoginButton, didCompleteWith result: LoginManagerLoginResult?, error: Error?) {
         if result != nil {
             if let grantedPermissions = result?.grantedPermissions, grantedPermissions.contains("email") {
                 fetchFacebookUserProfile()
+                // TODO: Handle publishing content with the "email" permission
             } else {
                 print("Email permission not granted.")
             }
@@ -100,6 +103,7 @@ class MainVC: UIViewController, GIDSignInDelegate, LoginButtonDelegate {
             print("Facebook login cancelled.")
         }
     }
+    
 }
 
 extension MainVC: ASAuthorizationControllerDelegate {
@@ -108,9 +112,9 @@ extension MainVC: ASAuthorizationControllerDelegate {
             let userID = appleIDCredential.user
             let email = appleIDCredential.email
             let fullName = appleIDCredential.fullName
-
+            
             let name = "\(fullName?.givenName ?? "") \(fullName?.familyName ?? "")"
-
+            
             let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
             let destinationVC = storyBoard.instantiateViewController(withIdentifier: "DetailVC") as! DetailVC
             destinationVC.name = name
@@ -120,7 +124,7 @@ extension MainVC: ASAuthorizationControllerDelegate {
             navigationController?.pushViewController(destinationVC, animated: true)
         }
     }
-
+    
     func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
         print("Apple sign-in error: \(error.localizedDescription)")
     }
